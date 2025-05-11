a26c4660c4b77cfa7999da3a35577f0552eb3b03
irfanzainudin <m.irfan.zain@gmail.com>
2025-05-11 14:37:14 +0800

    automate tests, but with incorrect test cases

--- tests.py
+++ tests.py
@@ -3,13 +3,25 @@ import enlighten
 from penghurai import hurai
 from senarai_kata import senarai_kata, pembahagian_suku_kata_untuk_senarai_kata as psk
 
-jumlah_kata = len(senarai_kata)
+# You may need to change color settings
+class Colours: 
+    RED = '\033[31m'
+    ENDC = '\033[m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+
+JUMLAH_KATA = len(senarai_kata)
 
 manager = enlighten.get_manager()
-progress_bar = manager.counter(total=jumlah_kata, desc='Testing', unit='ticks')
+progress_bar = manager.counter(total=JUMLAH_KATA, desc='Testing', unit='tests')
 
 for i, kata in enumerate(senarai_kata):
     if hurai(kata) != psk[i]:
-        print("WRONG")
-    time.sleep(0.01)
+        print(Colours.RED + "FAILED" + Colours.ENDC)
+    else:
+        print(Colours.GREEN + "PASSED" + Colours.ENDC)
+    # To slow down time and appreciate the beauty
+    # that is the art of testing your software
+    time.sleep(0.001)
     progress_bar.update()
